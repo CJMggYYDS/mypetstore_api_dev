5e449d7bf7bbdbe0443e76dca6b5b3e0b785f399
chen-jm <chenjmcjm@foxmail.com>
2022-07-29 15:53:24 +0800

    init project

---
+++ src/main/java/com/csucjm/mypetstore_api_dev/MypetstoreApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.csucjm.mypetstore_api_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreApiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreApiDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csucjm/mypetstore_api_dev/MypetstoreApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csucjm.mypetstore_api_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreApiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
